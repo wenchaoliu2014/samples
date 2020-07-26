b5bbcaed58b38acfe394b334a15d79fe9befdd93
wenchaoliu@foxmail.com <liuwenchao19>
2020-07-26 12:45:22 +0800

    as

---
+++ Clock_as/Clock/build/generated/source/buildConfig/release/com/winco/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.winco.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.winco.test";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
