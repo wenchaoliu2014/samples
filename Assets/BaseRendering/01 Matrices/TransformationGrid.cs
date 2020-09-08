36a31bc8dd244bbe766a2514bf392c92691e7219
wenchaoliu@foxmail.com <liuwenchao19>
2020-09-08 10:28:46 +0800

    xyz矩阵转换

--- Assets/BaseRendering/01 Matrices/TransformationGrid.cs
+++ Assets/BaseRendering/01 Matrices/TransformationGrid.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class TransformationGrid : MonoBehaviour {
 
@@ -7,6 +8,8 @@ public class TransformationGrid : MonoBehaviour {
 	public int gridResolution = 10;
 
 	Transform[] grid;
+
+	List<Transformation> transformations;
 
 	void Awake () {
 		grid = new Transform[gridResolution * gridResolution * gridResolution];
@@ -17,6 +20,7 @@ public class TransformationGrid : MonoBehaviour {
 				}
 			}
 		}
+		transformations = new List<Transformation>();
 	}
 
 	Transform CreateGridPoint (int x, int y, int z) {
@@ -36,5 +40,24 @@ public class TransformationGrid : MonoBehaviour {
 			y - (gridResolution - 1) * 0.5f,
 			z - (gridResolution - 1) * 0.5f
 		);
+	}
+
+	void Update () {
+		GetComponents<Transformation>(transformations);
+		for (int i = 0, z = 0; z < gridResolution; z++) {
+			for (int y = 0; y < gridResolution; y++) {
+				for (int x = 0; x < gridResolution; x++, i++) {
+					grid[i].localPosition = TransformPoint(x, y, z);
+				}
+			}
+		}
+	}
+
+	Vector3 TransformPoint (int x, int y, int z) {
+		Vector3 coordinates = GetCoordinates(x, y, z);
+		for (int i = 0; i < transformations.Count; i++) {
+			coordinates = transformations[i].Apply(coordinates);
+		}
+		return coordinates;
 	}
 }
